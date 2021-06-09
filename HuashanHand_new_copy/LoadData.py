1d415d910a0f92e6fd957f8d76a0f2ab54326233
Zhaobo Xu <zhaobo.xu@tum.de>
2021-06-09 23:09:56 +0200

    add the traning and testing phase

--- HuashanHand_new_copy/LoadData.py
+++ HuashanHand_new_copy/LoadData.py
@@ -1,4 +1,6 @@
 import os
+from typing import Any
+
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader, Dataset, random_split
@@ -27,27 +29,59 @@ class CustomDataSet(Dataset):
         self.labels = self.label_info['Kangfu'].tolist()
 
         self.data_dir = data_dir
+
+        for PID in self.PIDs:
+            file_name = os.path.join(self.data_dir, f'ROISignals_sub10{PID}.mat')
+            if not os.path.exists(file_name):
+                del self.labels[self.PIDs.index(PID)]
+                self.PIDs.remove(PID)
+
         self.transform = transform
 
     def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, idx):
-        PID = str(self.PIDs[idx])
+        PID = str(self.PIDs[idx]).zfill(3)
         file_name = os.path.join(self.data_dir, f'ROISignals_sub10{PID}.mat')
 
+        # some PID has no corresponding file
+
         signal = loadmat(file_name)
+        signal = signal['ROISignals']
+        signal = torch.from_numpy(signal).unsqueeze(0)
+
         label = self.labels[idx]
-        sample = {'img': signal, 'label': label}
+        sample = {'signal': signal, 'label': label}
         if self.transform:
             sample = self.transform(sample)
 
         return sample
 
 
+def load_data(input_dir: str, label_file_name, data_dir):
+    dataset = CustomDataSet(input_dir, label_file_name, data_dir, transform=None)
+    print('Data Loaded with total number of {} samples.'.format(len(dataset)))
+
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_set, test_set = torch.utils.data.random_split(dataset, [train_size, test_size])
+    print('Split into train: {} and test: {}.'.format(train_size, test_size))
+
+    train_loader = DataLoader(train_set, batch_size=5, drop_last=False, shuffle=True, num_workers=2)
+    test_loader = DataLoader(test_set, batch_size=5, drop_last=False, shuffle=False, num_workers=2)
+
+    return train_loader, test_loader
+
+
 if __name__ == "__main__":
     input_dir = os.getcwd()
-    label_file_name = 'HuashanHand_new_copy/Data/ROISignals_FunRawRWSDCF/Patient_Information_2019_09_29.xlsx'
-    data_dir = os.path.abspath('./HuashanHand_new_copy/Data/ROISignals_FunRawRWSDCF')
+    label_file_name = os.path.join('Data', 'ROISignals_FunRawRWSDCF', 'Patient_Information_2019_09_29.xlsx')
+    data_dir = os.path.join(os.getcwd(), 'Data', 'ROISignals_FunRawRWSDCF')
 
-    dataset = CustomDataSet(input_dir, label_file_name, data_dir, transform=None)
+    train_loader, test_loader = load_data(input_dir, label_file_name, data_dir)
+
+    for i, data in enumerate(test_loader):
+        signal = data['signal']
+        label = data['label']
+        print('num:{}| signal:{}| label:{}'.format(i, signal.size(), label))
--- HuashanHand_new_copy/TorchModel.py
+++ HuashanHand_new_copy/TorchModel.py
@@ -1,8 +1,16 @@
-import numpy as np
+import os
+from matplotlib import pyplot as plt
 import torch
+import torchvision
 import torch.optim as optim
 from torch import nn
 import torch.nn.functional as F
+from LoadData import load_data
+
+INPUT_DIR = os.getcwd()
+LABEL_FILE = 'Data\\ROISignals_FunRawRWSDCF\\Patient_Information_2019_09_29.xlsx'
+DATA_DIR = os.path.abspath('.\\Data\\ROISignals_FunRawRWSDCF')
+SAVE_PATH = '.\\trained_model.pth'
 
 
 class Conv2dNet(nn.Module):
@@ -12,9 +20,9 @@ class Conv2dNet(nn.Module):
         self.conv1 = nn.Conv2d(1, 10, 5)
         self.pool = nn.MaxPool2d(5, 5)
         self.conv2 = nn.Conv2d(10, 20, 5)
-        self.fc1 = nn.Linear(20*5*5, 500)
-        self.fc2 = nn.Linear(500, 125)
-        self.fc3 = nn.Linear(125, 20)
+        self.fc1 = nn.Linear(480, 200)
+        self.fc2 = nn.Linear(200, 75)
+        self.fc3 = nn.Linear(75, 15)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -27,33 +35,64 @@ class Conv2dNet(nn.Module):
 
         return x
 
-def train():
-    # not yet revised
-    net = Conv2dNet()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    for epoch in range(2):  # loop over the dataset multiple times
+def train(train_loader):
+    net = Conv2dNet().float()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=1e-3, weight_decay=0.01)
+
+    for epoch in range(50):  # loop over the dataset multiple times
 
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+        for i, data in enumerate(train_loader):
             # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
+            inputs = data['signal']
+            labels = data['label']
 
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs = net(inputs)
+            outputs = net(inputs.float())
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:  # print every 2000 mini-batches
+            if i % 5 == 4:  # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
+                      (epoch + 1, i + 1, running_loss / 5))
                 running_loss = 0.0
 
     print('Finished Training')
+    torch.save(net.state_dict(), SAVE_PATH)
+
+
+def test(test_loader):
+    net = Conv2dNet()
+    net.load_state_dict(torch.load(SAVE_PATH))
+
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for data in test_loader:
+            signals = data['signal']
+            labels = data['label']
+
+            outputs = net(signals)
+
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+            100 * correct / total))
+
+
+if __name__ == "__main__":
+    classes = ('no', 'yes')
+    train_loader, test_loader = load_data(INPUT_DIR, LABEL_FILE, DATA_DIR)
+    train(train_loader)
+    test(test_loader)
